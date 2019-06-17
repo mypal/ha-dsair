24e1604e3b67991fe8aa3fd5f52c7653cc86db69
shancheng.wang <shancheng.wang@dianping.com>
2019-06-17 18:26:49 +0800

    test

--- custom_components/ds_air/test.py
+++ custom_components/ds_air/test.py
@@ -7,7 +7,9 @@ PORT = 8008
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     st = struct.pack('<BHB', 2, 0, 3)
+    print('packed')
     s.sendall(st)
+    print('sent')
     data = s.recv(1024)
     print(repr(data))
 
