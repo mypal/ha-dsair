4ddf6678b58e574f82de2c3e9f1c2b5ddd733b58
shancheng.wang <shancheng.wang@dianping.com>
2019-06-17 18:22:28 +0800

    test

---
+++ custom_components/ds_air/test.py
@@ -0,0 +1,10 @@
+import socket
+import struct
+
+HOST = '192.168.1.110'
+PORT = 8008
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    st = struct.pack('<BHB', 2, 0, 3)
+    print(st)
