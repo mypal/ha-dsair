e71f75da3669a5570991bb0d46d6273e5eef848f
shancheng.wang <shancheng.wang@dianping.com>
2019-06-19 22:09:02 +0800

    test

--- custom_components/ds_air/__main__.py
+++ custom_components/ds_air/__main__.py
@@ -10,17 +10,18 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     try:
         s.connect((HOST, PORT))
         s.sendall(HandShakeParam().to_string())
-        data = s.recv(1024)
-        print('data:')
-        print(repr(data))
-        while data:
-            res, buf = decoder(data)
-            print('result:')
-            print(res)
-            print(repr(res))
-            print(res.__dict__)
-            print('buffer:')
-            print(repr(buf))
-            data = buf
+        while True:
+            data = s.recv(1024)
+            print('data:')
+            print(repr(data))
+            while data:
+                res, buf = decoder(data)
+                print('result:')
+                print(res)
+                print(repr(res))
+                print(res.__dict__)
+                print('buffer:')
+                print(repr(buf))
+                data = buf
     finally:
         s.close()
